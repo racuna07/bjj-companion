15353d00c6ecbb90cd7cb644479bd77b07889619
Rodrigo Acuña <rodrigo.acuna.f@gmail.com>
2023-04-17 11:47:51 -0600

    Added private user feed

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-
-import { api } from "~/utils/api";
 import Header from "~/components/layout/header";
 import { useUser } from "@clerk/nextjs";
 import UserFeed from "~/components/feed/user-feed";
